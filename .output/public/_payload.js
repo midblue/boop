057efd09c632104b63a103bc91b4b2dab287705e
Jasper Stephenson <jasperstephenson@gmail.com>
2023-04-26 15:22:43 +0900

    build

--- .output/public/_payload.js
+++ .output/public/_payload.js
@@ -1,1 +1,1 @@
-export default {data:{},prerenderedAt:1682490054248}
+export default {data:{},prerenderedAt:1682490163169}
--- .output/public/test/_payload.js
+++ .output/public/test/_payload.js
@@ -1,1 +1,1 @@
-export default {data:{},prerenderedAt:1682490054292}
+export default {data:{},prerenderedAt:1682490163208}
